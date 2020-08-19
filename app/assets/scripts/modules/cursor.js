d09966e8625d8aee047128514cc8341875f72588
Khan Mohsin <m90khan@gmail.com>
2020-08-19 10:53:20 +0500

    update final setup

--- app/assets/scripts/modules/cursor.js
+++
@@ -1,7 +0,0 @@
-const mouse = document.querySelector(".cursor");
-
-window.addEventListener("mousemove", cursor);
-function cursor(e) {
-  mouse.style.top = e.pageY + "px";
-  mouse.style.left = e.pageX + "px";
-}
